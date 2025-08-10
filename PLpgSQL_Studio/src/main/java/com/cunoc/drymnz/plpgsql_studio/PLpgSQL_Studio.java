f16aa3289f65fc4d9413f005f4f35010b08a4d0b
Benjamìn de Jesùs <mcdreck276@gmail.com>
2025-08-10 01:08:22 -0600

    inicio de proyecto

---
+++ PLpgSQL_Studio/src/main/java/com/cunoc/drymnz/plpgsql_studio/PLpgSQL_Studio.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.cunoc.drymnz.plpgsql_studio;
+
+/**
+ *
+ * @author drymnz
+ */
+public class PLpgSQL_Studio {
+
+    public static void main(String[] args) {
+         /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> new InicioJFrame().setVisible(true));
+    }
+}
